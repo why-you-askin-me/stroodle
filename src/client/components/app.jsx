80627758f6d7f66958ef26f91e963a7574fded6f
Alex <alexzhou.645@gmail.com>
2016-11-18 17:20:11 -0500

    We are screwed

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const App = () => (
-    <p>Suh dude</p>
+    <div>
+    <h1>Suh dude</h1>
+    
+    <p>It feel like we are screwed for this assignment</p>
+    </div>
 )
 
 export default App
