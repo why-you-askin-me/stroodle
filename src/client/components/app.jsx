e04cfc60137839f5eb1303eec06f1ef101c722e1
thumber <thumber@scs.ryerson.ca>
2016-11-19 15:16:34 -0500

    Added formatting for tutorial steps

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -2,10 +2,27 @@
 import React from 'react'
 
 const App = () => (
-    <div>
-    <h1>Suh dude</h1>
-    
-    <p>It feel like we are screwed for this assignment</p>
+    <div class="container">
+        <div class="pg_header">
+            <h1>Welcome to the club.</h1>
+            <h2>Making a simple component with React</h2>
+        </div>
+        <div class="wrapper">
+            <div class="wrapper_header">
+                <h1>Step 1:</h1>
+            </div>
+            <div class="wrapper_body">
+                <p>This is where you put the meat of how you do 'Step 1'</p>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="wrapper_header">
+                <h1>Step 2:</h1>
+            </div>
+            <div class="wrapper_body">
+                <p>This is where you put the meat of how you do 'Step 2'</p>
+            </div>
+        </div>
     </div>
 )
 
